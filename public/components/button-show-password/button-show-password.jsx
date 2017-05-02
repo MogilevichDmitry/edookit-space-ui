ab183e219ea61eb2b9e6bcf3819f308d0b745d06
Serhei Dudko <barcaman4ik@MacBook-Pro-Serhei.local>
2017-05-02 16:57:56 +0300

    Add show password icon

---
+++ public/components/button-show-password/button-show-password.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Icon from '../icon/icon.jsx';
+import IconShowPassword from '!raw-loader!../../resources/icons/icon-show-password.svg';
+import './button-show-password.less';
+
+const ButtonShowPassword = ({className}) => (
+  <button className={classnames('btn-show-password', className)}>
+    <Icon svg={IconShowPassword} />
+  </button>
+);
+
+ButtonShowPassword.propTypes = {
+  className: PropTypes.string
+}
+
+export default ButtonShowPassword;
