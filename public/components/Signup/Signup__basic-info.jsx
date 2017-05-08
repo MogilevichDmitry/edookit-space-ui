4970348d3707e05366690606fac76364cd2f930a
Serhei Dudko <barcaman4ik@MacBook-Pro-Serhei.local>
2017-05-08 07:09:35 +0300

    Fix signup forms

--- public/components/Signup/Signup__basic-info.jsx
+++ public/components/Signup/Signup__basic-info.jsx
@@ -2,43 +2,50 @@ import React from 'react';
 import Icon from '../icon/icon.jsx';
 import IconBasicInfo from '!raw-loader!../../resources/icons/icon-basic-info.svg';
 import InputField from '../input-field/input-field.jsx';
+import IconMail from '!raw-loader!../../resources/icons/icon-mail.svg';
+import IconFullName from '!raw-loader!../../resources/icons/icon-full-name.svg';
 import Button from '../button/button.jsx';
 import SocialButton from '../social-button/social-button.jsx';
 
 const SignupBasicInfo = () => (
   <div className="signup__basic">
-      <span className="signup__item-picture">
-        <Icon svg={IconBasicInfo}/>
-      </span>
-      <div className="signup__basic-main">
-        <form className="signup__basic-form">
-          <InputField
-            label="E-mail:"
-            placeholder="name@example.com"
-            type="email"
-          />
-          <InputField
-            label="Full name:"
-            placeholder="Your name"
-            type="email"
-          />
-          <Button className="signup__basic-form-button" modifier="primary">Next</Button>
-        </form>
-        <div className="signup__basic-socials">
-          <span className="signup__basic-socials-title">Or sign up with:</span>
-          <SocialButton
-            className="signup__basic-socials-item"
-            modifier="facebook"
-            text="Facebook"
-          />
-          <SocialButton
-            className="signup__basic-socials-item"
-            modifier="google"
-            text="Google"
-          />
-        </div>
+    <span className="signup__item-picture">
+      <Icon svg={IconBasicInfo}/>
+    </span>
+    <div className="signup__basic-main">
+      <form className="signup__basic-form">
+        <InputField
+          label="E-mail:"
+          placeholder="name@example.com"
+          type="email"
+          icon={IconMail}
+        />
+        <span className="signup__basic-form-description">
+          We promise you, we won't SPAM you!
+        </span>
+        <InputField
+          label="Full name:"
+          placeholder="Your name"
+          type="email"
+          icon={IconFullName}
+        />
+        <Button className="signup__basic-form-button" modifier="primary">Next</Button>
+      </form>
+      <div className="signup__basic-socials">
+        <span className="signup__basic-socials-title">Or sign up with:</span>
+        <SocialButton
+          className="signup__basic-socials-item"
+          modifier="facebook"
+          text="Facebook"
+        />
+        <SocialButton
+          className="signup__basic-socials-item"
+          modifier="google"
+          text="Google"
+        />
       </div>
     </div>
+  </div>
 );
 
 export default SignupBasicInfo;
--- public/components/Signup/Signup__school.jsx
+++ public/components/Signup/Signup__school.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Icon from '../icon/icon.jsx';
+import IconSchool from '!raw-loader!../../resources/icons/icon-school.svg';
+import InputField from '../input-field/input-field.jsx';
+import Button from '../button/button.jsx';
 
-const SignupSchool = () => (
+const SignupBasicInfo = () => (
   <div className="signup__school">
-    SCHOOL
+    <span className="signup__item-picture">
+      <Icon svg={IconSchool}/>
+    </span>
+    <h3 className="signup__school-title">
+      You are almost done.
+    </h3>
+    <form className="signup__school-form">
+      <InputField
+        label="Your school:"
+        placeholder="Select Your School"
+        type="select"
+      />
+      <InputField
+        label="Your role:"
+        placeholder="Select a role"
+        type="select"
+      />
+      <Button className="signup__school-form-button" modifier="primary">Complete</Button>
+    </form>
   </div>
 );
 
-export default SignupSchool;
+export default SignupBasicInfo;
